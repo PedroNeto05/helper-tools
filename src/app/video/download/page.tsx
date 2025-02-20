30815597fcbe35f2f0473d8a1e16527c9f9bd23d
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-02-20 08:00:10 -0300

    ref: move form to component

--- src/app/video/download/page.tsx
+++ src/app/video/download/page.tsx
@@ -1,94 +1,54 @@
 'use client';
 import { Card } from '@/components/ui/card';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
 import { useState } from 'react';
-import { LoaderCircle } from 'lucide-react';
-
-const formSchema = z.object({
-  url: z.string().url(),
-});
+import { DownloadVideoForm } from '@/components/download-video-form';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
 
 export default function VideoDownloader() {
-  const [isFeatching, setIsFeatching] = useState<boolean>(false);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
   const [validUrl, setValidUrl] = useState<boolean>(false);
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      url: '',
-    },
-  });
+  const [audioOnly, setAudioOnly] = useState<boolean>(false);
+  const [InvalidUrlDialog, setInvalidUrlDialog] = useState<boolean>(false);
+
   return (
     <div className='flex h-full space-x-5'>
       <div className='flex flex-1 flex-col space-y-5'>
-        <Form {...form}>
-          <Card className='flex flex-1 items-center p-4'>
-            <FormField
-              control={form.control}
-              name='url'
-              render={({ field }) => (
-                <FormItem className='w-full'>
-                  <FormControl>
-                    <div className='flex w-full space-x-5'>
-                      <Input
-                        {...field}
-                        placeholder='https://exemplo.com'
-                        className='p-6'
-                      />
-                      <Button type='button' className='cursor-pointer p-6'>
-                        {isFeatching ? (
-                          <LoaderCircle className='animate-spin' />
-                        ) : (
-                          'Pesquisar'
-                        )}
-                      </Button>
-                    </div>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </Card>
-          <Card className='flex-10 p-4'>
-            <FormField
-              control={form.control}
-              name='quality'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Qualidade do Vídeo</FormLabel>
-                  <FormControl>
-                    <select
-                      {...field}
-                      className='w-full rounded-md border p-2'
-                      disabled={!validUrl}
-                    >
-                      <option value=''>Selecione a qualidade</option>
-                    </select>
-                  </FormControl>
-                  <FormDescription>
-                    Selecione a qualidade desejada para o download
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </Card>
-        </Form>
+        <DownloadVideoForm
+          audioOnly={audioOnly}
+          isFetching={isFetching}
+          setAudioOnly={setAudioOnly}
+          validUrl={validUrl}
+          setValidUrl={setValidUrl}
+          setIsFetching={setIsFetching}
+          setInvalidUrlDialog={setInvalidUrlDialog}
+        />
       </div>
       <Card className='flex-1 p-4'>
         <h1>fila de downloads e baixar a fila</h1>
       </Card>
+      <Dialog open={InvalidUrlDialog} onOpenChange={setInvalidUrlDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Erro ao buscar vídeo</DialogTitle>
+          </DialogHeader>
+          <div className='space-y-4'>
+            <p className='text-red-500'>URL inválida</p>
+            <Button
+              variant='destructive'
+              onClick={() => setInvalidUrlDialog(false)}
+              className='cursor-pointer'
+            >
+              Fechar
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
