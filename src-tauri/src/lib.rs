17d1de0f646cb88ab2a8488bc161fe1c04d67ac9
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-02-23 00:08:20 -0300

    feat: add dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,12 @@
 mod video_downloader;
-use video_downloader::{validate_url,get_video_info};
+use video_downloader::{get_video_info, validate_url};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![validate_url,get_video_info])
+        .invoke_handler(tauri::generate_handler![validate_url, get_video_info])
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
